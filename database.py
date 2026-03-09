47d1fbb16ddc5495aae7c323de5fb2906f2aadbc
João Pedro Soares <joao698353@gmail.com>
2026-03-09 08:40:07 -0300

    init

---
+++ database.py
@@ -0,0 +1,35 @@
+from flask_login import UserMixin
+from sqlalchemy import create_engine, String, Integer, func, Column, DateTime, ForeignKey, Numeric, Float
+from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
+from sqlalchemy.exc import SQLAlchemyError, SQLAlchemyError
+from unicodedata import numeric
+from werkzeug.security import generate_password_hash, check_password_hash
+
+# base de dados
+engine = create_engine('mysql+pymysql://root:senaisp@localhost:3306/empresa_db')
+
+db_session = scoped_session(sessionmaker(bind=engine))
+Base = declarative_base()
+Base.query = db_session.query_property()
+
+
+class Funcionario(Base, UserMixin):
+    __tablename__ = 'funcionarios'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    nome = Column(String, nullable=False)
+    data_nascimento = Column(DateTime, nullable=False)
+    cpf = Column(String(11), nullable=False, unique=True)
+    email = Column(String(120), nullable=False, unique=True)
+    senha = Column(String(255), nullable=False)
+    cargo = Column(String(120), nullable=False)
+    salario = Column(Float, nullable=False)
+    criado_em = Column(DateTime, server_default=func.now(), nullable=False)
+
+    def __repr__(self):
+        return f'<Funcionario {self.nome}>'
+
+    def set_password(self, password):
+        self.senha = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.senha, password)
